8fed6159e04b1bbb717d48236697d5cd2ad6fe01
alvaroatue <163065658+alvaroatue@users.noreply.github.com>
2025-01-03 17:35:07 -0300

    a
    a

--- src/main/java/com/example/backend_challenge/Services/JwtService.java
+++ src/main/java/com/example/backend_challenge/Services/JwtService.java
@@ -3,13 +3,50 @@ package com.example.backend_challenge.Services;
 import com.example.backend_challenge.Entities.UserEntity;
 import lombok.Value;
 
-public class JwtService implements JwtSer {
+public class JwtService  {
 
+    private String secret = "your_secret_key";
+    private long jwtExpiration = 1000 * 60 * 60; // 1 hour
+    private long refreshExpiration = 1000 * 60 * 60 * 24; // 1 day
 
-    private String secret;
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
 
-    private long jwtExpiration;
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
 
-    private long refreshExpiration;
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
 
+    public String generateToken(UserEntity userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserEntity userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 }
